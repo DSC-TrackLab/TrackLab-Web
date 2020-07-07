d93edb927ff144d9916ce00524500e8550629ff6
SabeloX <sabelomtw@gmail.com>
2020-07-07 22:38:35 +0200

    Touch up of Register and Login Views

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,21 +2,30 @@
 
 @section('content')
 <div class="container">
+
+    <!--Register Form Box-->
     <div class="row">
+
+        <!--Description box-->
         <div class="intro-msg align-self-center col-md-6">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora facilis, modi amet ratione necessitatibus laborum repellendus, perspiciatis saepe id dolores ipsam ab aliquid! Impedit non mollitia ipsam? Nostrum, voluptatibus tenetur.
         </div>
+
+        <!--Form box-->
         <div class="align-self-center form col-md-6">
 
             <p class="text-center">#LOGO HERE</p>
+
             <h3 class='text-center'>Sign up</h3>
+
+            <!--Form-->
             <form method="POST" action="{{ route('register') }}">
-                @csrf
-                
+                @csrf  
+
                 <!-- Full Name Input -->
                 <div class="input row-md-6">
                     <label for="name">Full Name</label>
-                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required>
                     
                     @error('name')
                         <span class="invalid-feedback" role="alert">
@@ -28,7 +37,7 @@
                 <!-- Password Input -->
                 <div class="input row-md-6">
                     <label for="password">Password</label>
-                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required>
 
                     @error('password')
                         <span class="invalid-feedback" role="alert">
@@ -48,33 +57,29 @@
                         </span>
                     @enderror
                 </div>
-                <!--<div class="form-group row">
 
-                    <div class="col-md-6">
-                        <label for="password-confirm">{{ __('Confirm Password') }}</label>
-                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                    </div>
-                </div>-->
-
-                <!-- Radios -->
+                <!-- Radios for registration options -->
                 <div class='radio-options'>
+
                     <h5>Sign up as:</h5>
-                    <input type="radio" name="driver" id="driver" value="option1">
+
+                    <input type="radio" name="driver" id="driver">
                     <label class="radio-inline">Courier/Driver</label>
                     
-                    <input type="radio" name="customer" id="customer" value="option2">
+                    <input type="radio" name="customer" id="customer">
                     <label class="radio-inline">Customer</label>
 
-                    <input type="radio" name="guest" id="guest" value="option3">
+                    <input type="radio" name="guest" id="guest">
                     <label class="radio-inline">Guest</label>
+
                 </div>
 
                 <div class="text-center">
-                    <button type="submit" class="btn btn-success">Create Account</button>
+                    <button type="submit" class="btn btn-primary">Create Account</button>
                 </div>
 
                 <div class='text-center'>
-                    <p>Already have an account? <a href=''>Log in</a></p>
+                <p>Already have an account? <a href='{{route('login')}}'>Log in</a></p>
                 </div>
             </form>
     </div>
