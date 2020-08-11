8867d4db34583e5cea5a7af6f067c4c1befe94a3
Matome <Matome>
2020-08-11 13:24:40 +0200

    previous commit was a user_types, this is the real commit of the user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('user_type_id')->nullable();
             $table->string('name');
+            $table->string('surname')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('mobile_number')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
