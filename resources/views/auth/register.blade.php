a837b370da22481f078991227d664d1aba5a1ab0
SabeloX <sabelomtw@gmail.com>
2020-08-12 17:15:01 +0200

    Selection Vehicle view and controller partially complete

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -19,7 +19,7 @@
             <h3 class='text-center'>Sign up</h3>
 
             <!--Form-->
-            <form method="POST" action="{{ route('register') }}">
+            <form method="POST" action="{{route('register')}}">
                 @csrf  
 
                 <!-- Full Name Input -->
@@ -66,27 +66,31 @@
                 </div>
 
                 <!-- Radios for registration options -->
-                <div class='radio-options'>
+                <div class='form-group'>
 
                     <h5>Sign up as:</h5>
 
-                    <input type="radio" name="driver" id="driver">
-                    <label class="radio-inline">Courier/Driver</label>
-                    
-                    <input type="radio" name="customer" id="customer">
-                    <label class="radio-inline">Customer</label>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="options" id="driver" value="option1">
+                        <label class="form-check-label" for="driver">Courier/Driver</label>
+                    </div>
 
-                    <input type="radio" name="guest" id="guest">
-                    <label class="radio-inline">Guest</label>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="options" id="customer" value="option2">
+                        <label class="form-check-label" for="customer">Customer</label>
+                    </div>
 
+                    <div class="form-check form-check-inline">
+                      <input class="form-check-input" type="radio" name="options" id="guest" value="option3">
+                      <label class="form-check-label" for="guest">Guest</label>
+                  </div>
                 </div>
-
                 <div class="text-center">
                     <button type="submit" class="btn btn-primary">Create Account</button>
                 </div>
 
                 <div class='text-center'>
-                <p>Already have an account? <a href='{{route('login')}}'>Log in</a></p>
+                    <p>Already have an account? <a href='{{route('login')}}'>Log in</a></p>
                 </div>
             </form>
     </div>
